858e7a6b352c638abe96638d91115ca1269c54fd
fillbertRiko <147192155+fillbertRiko@users.noreply.github.com>
2025-01-19 22:27:50 +0700

    init
    Upload code

---
+++ InRaMang/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InRaMang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printArr();
+            Console.ReadLine();
+        }
+
+        static void printArr()
+        {
+            int i, j;
+            int[,] tArr = new int[5, 5];
+            for (i = 0; i < 5; i++) //Khởi gán các giá trị tới mảng 2 chiều
+                for (j = 0; j < 5; j++)
+                {
+                    if (i == 0)
+                        tArr[i, j] = j + 1; //Điền vào hàng đầu tiên
+                    else if (i > 0 && j == 0)
+                        tArr[i, j] = tArr[i - 1, 4] + 1; //lấy giá trị của ô sau cùng trong hàng trước 
+                    else
+                        tArr[i, j] = tArr[i, j - 1] + 1; //Điền vào các ô tiếp theo
+                }
+            for (i=0;i<5;i++)
+            {
+                for(j=0;j<5;j++)
+                {
+                    Console.WriteLine("{0}\t",tArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SapXepMangGiamDan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SapXepMangGiamDan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 23, 2, 3, 34, 6, 1, 24, 45, 78, 8 }; //tập dữ liệu chưa sắp xếp
+            bubblesort(arr, 10); //gọi thủ tục sắp xếp nổi bọt
+            int i;
+            for (i = 0; i < 10; i++)
+                Console.Write(arr[i] + "\t"); //in ra kết quả theo thứ tự tăng dần
+            Console.ReadLine();
+        }
+        ///Thuật toán nổi bọt
+        static void bubblesort(int[] dataset, int n)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+                for (j = n - 1; j > i; j--)
+                    if (dataset[j] > dataset[j - 1])
+                    {
+                        int temp = dataset[j-1];
+                        dataset[j-1] = dataset[j];
+                        dataset[j] = temp;
+                    }
+        }
+    }
+}
