d02ff422895eba6d5159d77f0430641a3316202f
fillbertRiko <147192155+fillbertRiko@users.noreply.github.com>
2025-02-06 15:56:44 +0700

    init

---
+++ baiToanQuanHau/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baiToanQuanHau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 5, n = 5;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
